eee56eab261c6ea04273237448b3358375ecd9ef
Star7-jpg <marcosconde612@gmail.com>
2025-04-02 21:38:59 -0600

    10 Book property

--- djangobnb_backend/useraccount/models.py
+++ djangobnb_backend/useraccount/models.py
@@ -4,19 +4,19 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
 
-# Create your models here.
+
 class CustomUserManager(UserManager):
     def _create_user(self, name, email, password, **extra_fields):
         if not email:
-            raise ValueError("You have not specified a valid email address")
-        
+            raise ValueError("You have not specified a valid e-mail address")
+    
         email = self.normalize_email(email)
         user = self.model(email=email, name=name, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
         return user
-    
+
     def create_user(self, name=None, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
@@ -26,8 +26,9 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(name, email, password, **extra_fields)
-    
-class  User(AbstractBaseUser, PermissionsMixin):
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255, blank=True, null=True)
@@ -37,8 +38,8 @@ class  User(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    date_joined = models.DateField(auto_now_add=True)
-    last_login = models.DateField(blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(blank=True, null=True)
 
     objects = CustomUserManager()
 
@@ -50,4 +51,4 @@ class  User(AbstractBaseUser, PermissionsMixin):
         if self.avatar:
             return f'{settings.WEBSITE_URL}{self.avatar.url}'
         else:
-            ''
+            return ''
